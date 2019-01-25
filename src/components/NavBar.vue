e6fb3957041c2bef039e2bac303fcd702791f2d7
XuYi <MyXOF@users.noreply.github.com>
2019-01-26 01:05:23 +0800

    fix nav button disappear bug and sample data url wrong (#10)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,6 +4,13 @@
       <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <a class="navbar-brand" href="#/">IoTDB</a>
         </div>
 
