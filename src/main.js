e223b5d5390bed1f6c276d9e6f508b9957ef0083
Sail <37333508+SailVR@users.noreply.github.com>
2020-03-10 16:18:58 +0800

    [IOTDB-521]modify home and document (#67)
    * modify home and document
    
    * modify home and document

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App'
 import router from './router'
 import global_ from './components/Global'
 import $ from 'jquery'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
 Vue.prototype.GLOBAL = global_
+
 
 /* eslint-disable no-new */
 new Vue({
