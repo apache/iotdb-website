08fe7bba11fdc5e194df43ce63a3261e3dc66189
Sail <37333508+SailVR@users.noreply.github.com>
2020-03-11 15:23:04 +0800

    update home & document & docs/0.9 (#69)
    * create local file
    
    * modify home & document & 0.9/4.2&4.3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template >
   <div style="background:linear-gradient(top,#A2A2A2,#fff);">
 
-    <div  style="max-width: 1200px;margin: 0 auto;position: relative;height:450px;border-bottom:1px solid gray;">
+    <div  style="width:74%;margin: 0 auto;position: relative;height:450px;border-bottom:1px solid gray;">
       
       <h2 class="h2">Apache IoTDB</h2>
       <p style="font-size: 20px;line-height:23px;margin: 10px 0 20px 0;font-family: 'Arimo', sans-serif;
@@ -27,7 +27,7 @@
       </el-row>
     </div>
 
-        <div style="max-width:1200px;margin:0 auto;text-align: center;margin-top:20px;">
+        <div style="width:60%;margin:0 auto;text-align: center;margin-top:20px;">
             <p class="home-title" style="font-size: 50px;">Scenarios</p>
             <div id="myCarousel" class="carousel slide" data-ride="carousel" style="background: rgb(31,31,31)">
               <ol class="carousel-indicators">
@@ -85,129 +85,23 @@
             </div>
           </div>
 
-          <div  style="max-width:1200px;margin:0 auto;text-align: center;margin-top:20px;border-top:1px solid gray;">
+          <div  style="width:74%;margin:0 auto;text-align: center;margin-top:20px;border-top:1px solid gray;">
             <p class="home-title" style="padding-top:15px;font-size: 54px;">Main Features</p>
             <div class="row feature">
               <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 feature-item" v-for="item in featureBlock1" @mouseover="hover($event)"
-                   @mouseleave="unhover($event)">
-                <div class="row feature-title-row">
+                   @mouseleave="unhover($event)" >
+                <div class="row feature-title-row" style="margin-top:60px;">
                   <div class="col-xs-2 col-sm-2">
-                    <img :src=item.featureSrc style="vertical-align:middle">
+                    <img :src=item.featureSrc style="vertical-align:middle;margin-left:0px;">
                   </div>
-                  <div class="col-xs-10 col-sm-10 feature-title"><p>{{item.featureTitle}}</p></div>
+                  <div class="col-xs-10 col-sm-10 feature-title" style="vertical-align:middle;"><p>{{item.featureTitle}}</p></div>
                 </div>
-                  <p class="feature-content">{{item.featureContent}}</p>
+                <p class="feature-content" id="content" style="display:none;">{{item.featureContent}}</p>
                 <hr/>
               </div>
             </div>
           </div>
 
-
-
-    <div class="main container">
-      <div class="row">
-        <div class="col-md-9 col-sm-9">
-
-          
-          <!-- <div>
-            <p class="home-title">Apache IoTDB (incubating) (Database for Internet of Things)</p>
-            <article lang="en">
-              <p>Apache IoTDB (incubating) (Database for Internet of Things) is an integrated data
-                management engine designed for timeseries data. It provides users with services for
-                data collection, storage and analysis. Due to its light-weight architecture, high
-                performance and rich feature set together with its deep integration with Apache
-                Hadoop and Spark, Apache IoTDB (incubating) can meet the requirements of massive
-                data storage, high-speed data ingestion and complex data analysis in the IoT
-                industrial fields.
-              </p>
-              <p>
-                Apache IoTDB (incubating) was initially developed by Prof. Wang's Team of School of
-                Software at Tsinghua University. Now it is an effort undergoing incubation at The
-                Apache Software Foundation (ASF), sponsored by the Incubator.
-              </p>
-            </article>
-          </div> -->
-
-          <!-- <div>
-            <p class="home-title">Scenarios</p>
-            <div id="myCarousel" class="carousel slide" data-ride="carousel" style="background: rgb(31,31,31)">
-              <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-              </ol>
-              <div class="carousel-inner">
-                <div class="item active">
-                  <img src="https://user-images.githubusercontent.com/1021782/50463652-f67c6480-09c7-11e9-8f65-c8adaff09043.png" alt="First slide">
-                  <br/>
-                  <div class="Scenarios">
-                    <h1>High-end equipment</h1>
-                    <article lang="en">
-                      <p> In high-end manufacturing, there are many devices equipped with sensors collecting working status data.
-                        Meteorological stations, wind turbines are common high-end devices.
-                        These devices, if supports Java or Go (in developing), can run TsFile to store data locally.
-                        In this way, TsFile can provide the ability to save data with high throughput, high compression ratio, and millisecond query.
-                        Together with TsFile-Sync tool, TsFiles can be transmitted to the data center.
-                      </p>
-                    </article>
-                  </div>
-                </div>
-                <div class="item">
-                  <img src="https://user-images.githubusercontent.com/1021782/50463651-f67c6480-09c7-11e9-9374-3e6c6c540d7b.png" alt="Second slide">
-                  <br/>
-                  <div class="Scenarios">
-                    <h1>Local Controller</h1>
-                    <article lang="en">
-                      <p>
-                        In the scene of factories, there are tens of devices under LAN network.
-                        IoTDB can be installed on a local controller server in the factory to receive data from those devices.
-                        The local controller server (normal PC or workstation) with IoTDB can provide the ability to persist data and query data with SQL-like interface.
-                        In addition, with TsFile-Sync tool, TsFiles on the local controller can be transmitted to the data center equipped with IoTDB instance in the cloud.
-                      </p>
-                    </article>
-                  </div>
-                </div>
-                <div class="item">
-                  <img src="https://user-images.githubusercontent.com/1021782/50463650-f67c6480-09c7-11e9-9b80-bc66aa5dc0ab.png" alt="Third slide">
-                  <br/>
-                  <div class="Scenarios">
-                    <h1>Cloud Data Management</h1>
-                    <article lang="en">
-                      <p>
-                        In the scene of high-speed network (Internet of Vehicles, etc.), a car installed sensors on it can collect monitoring information(driving status, etc.) of itself at a certain frequency.
-                        Usually, these automotive devices have limited hardware configurations and are difficult to carry complex applications.
-                        Lightweight IoTDB(IoTDB Client) came into being.
-                        With JDBC API, it can make data sent by narrow-band IoT or 4G possible. In this way, devices and cloud are connected together.
-                      </p>
-                    </article>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> -->
-
-          <!-- <div>
-            <p class="home-title" style="padding-top: 15px">Main Features</p>
-            <div class="row feature">
-              <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 feature-item" v-for="item in featureBlock1" @mouseover="hover($event)"
-                   @mouseleave="unhover($event)">
-                <div class="row feature-title-row">
-                  <div class="col-xs-2 col-sm-2">
-                    <img :src=item.featureSrc style="vertical-align:middle">
-                  </div>
-                  <div class="col-xs-10 col-sm-10 feature-title"><p>{{item.featureTitle}}</p></div>
-                </div>
-                  <p class="feature-content">{{item.featureContent}}</p>
-                <hr/>
-              </div>
-            </div>
-          </div> -->
-          
-        </div>
-        <!-- <side-bar/> -->
-      </div>
-
-    </div>
     <footer-bar/>
   </div>
 </template>
@@ -224,6 +118,7 @@
     data() {
       return {
         msg: 'Welcome to Home Page',
+        isHover:false,
         featureBlock1: [
           {
             featureSrc: require("../assets/img/home-icon1.png"), featureTitle: "High-throughput read and write",
@@ -254,10 +149,14 @@
     },
     methods: {
       hover: function (e) {
-        e.currentTarget.style.boxShadow = "2px 2px 10px #909090"
+        e.currentTarget.style.boxShadow = "2px 2px 10px #909090";
+        e.currentTarget.firstElementChild.style.marginTop = "0px";
+        e.currentTarget.firstElementChild.nextElementSibling.style.display = 'block';
       },
       unhover: function (e) {
         e.currentTarget.style.boxShadow = "";
+        e.currentTarget.firstElementChild.style.marginTop = "60px";
+        e.currentTarget.firstElementChild.nextElementSibling.style.display = 'none';
       },
       
       addRoutes1 () {
@@ -298,7 +197,7 @@
 
   @media (min-width: 768px) {
     .feature-item{
-      min-height: 200px;
+      min-height: 220px;
     }
     .carousel-inner{
       min-height: 520px;
@@ -307,7 +206,7 @@
 
   @media (min-width: 992px) {
     .feature-item{
-      min-height: 220px;
+      min-height: 240px;
     }
     .carousel-inner{
       min-height: 580px;
@@ -316,7 +215,7 @@
 
   @media (min-width: 1200px){
     .feature-item{
-      min-height: 195px;
+      min-height: 210px;
     }
     .carousel-inner{
       min-height: 650px;
@@ -435,7 +334,7 @@
 }
 
 h2 {
-    margin: 150px auto 0;
+    margin: 120px auto 0;
     padding: 0 5%;
     opacity: 0.9;
     color: #3f3b3f;
@@ -444,7 +343,7 @@ h2 {
 	  color: transparent; */
     font-size: 108px;
     text-align: center;
-    text-transform: uppercase;
+    text-transform: none;
     font-weight: 700;
     letter-spacing: 0.03em;
 }
